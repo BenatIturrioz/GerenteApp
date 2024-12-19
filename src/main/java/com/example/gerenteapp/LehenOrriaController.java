2cee0691462164189f66dfe8ac28f1be6c0eb1c8
BenatIturrioz <beitumu7@gmail.com>
2024-12-19 14:48:31 +0100

    langileak kudeatzeko orria ondo jartzen.

--- src/main/java/com/example/gerenteapp/LehenOrriaController.java
+++ src/main/java/com/example/gerenteapp/LehenOrriaController.java
@@ -1,7 +1,13 @@
 package com.example.gerenteapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class LehenOrriaController extends BaseController{
@@ -13,9 +19,25 @@ public class LehenOrriaController extends BaseController{
     private Button erreserbakButton;
 
     @FXML
-    public void handleLangileakKudeatu(){
+    private Label errorLabel;
 
+    private void aldatuEscenaLangileakKudeatu() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/gerenteapp/LangileakKudeatu.fxml"));
+            Scene escenaLehenOrria = new Scene(loader.load());
+
+            Stage stageActual = this.getUsingStage();
+
+            stageActual.setScene(escenaLehenOrria);
+            stageActual.setTitle("Langile Kudeaketa");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            errorLabel.setText("Ezin da orria kargatu :(");
+            errorLabel.setVisible(true);
+        }
     }
+
 
     @FXML
     public void handleErreserbakKudeatu(){
